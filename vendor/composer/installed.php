b2c5a6bc3831e82c03a4937a38c23008be5ebf1c
Julian Carelli <julianignaciocarelli@gmail.com>
2020-11-20 11:22:42 -0300

    Add new changes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,40 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'julix/chest-of-curriculums',
+  ),
+  'versions' => 
+  array (
+    'julix/chest-of-curriculums' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'twbs/bootstrap' => 
+    array (
+      'pretty_version' => 'v4.0.0',
+      'version' => '4.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8a628b943cf31ca0a002c08af661a95772480225',
+    ),
+    'twitter/bootstrap' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v4.0.0',
+      ),
+    ),
+  ),
+);
